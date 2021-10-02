ea50b9b63263138cebc7bb0cea3ae6832d46e1a0
heyunlei <heyunlei2000@163.com>
2021-10-02 15:19:13 +0800

    idea first commit

---
+++ src/main/java/com/heyunlei/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.heyunlei.git;
+
+/**
+ * @author HeYunLei
+ * @date 2021/10/2 - 15:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git.");
+    }
+}
